58b37c21fe417ed7765bdbb62addf9d48fd9b490
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-04-11 17:10:36 +1000

    Rename theme to Eclipse Dark

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use mottle::theme::{ThemeBuilder, Type};
 use std::io;
 
 fn main() -> io::Result<()> {
-    let mut eclipse = ThemeBuilder::new("Eclipse".to_string(), Type::Dark);
-    imp::add_rules(&mut eclipse, &colors::Dark);
-    eclipse.build().save()?;
+    let mut builder = ThemeBuilder::new("Eclipse Dark".to_string(), Type::Dark);
+    imp::add_rules(&mut builder, &colors::Dark);
+    builder.build().save()?;
 
     Ok(())
 }
